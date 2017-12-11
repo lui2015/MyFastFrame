c0dd9c3ce0fc3951af0051bec99d4c1ef6321630
luli <635003514@qq.com>
2017-12-11 19:02:22 +0800

    快速开发框架

---
+++ app/src/main/java/com/luli/myfastframe/base/Constants.java
@@ -0,0 +1,17 @@
+package com.luli.myfastframe.base;
+
+/**
+ * 常量
+ * Created by luli on 2017/12/11.
+ */
+
+public class Constants {
+
+    /**
+     * 日志相关常量
+     */
+    public static final String logRequest = "request";
+    public static final String logResponse = "response";
+    public static final String logDebug = "myDebug";
+    public static final String logError = "myError";
+}
